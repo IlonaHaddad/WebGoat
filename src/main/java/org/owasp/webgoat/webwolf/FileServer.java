32d9d238a3e8a6696ec79d625d0d2da9dacac089
OX Security <api@ox.security>
2026-07-28 21:03:31 +0000

    Fix java.spring.security.unrestricted-request-mapping in src/main/java/org/owasp/webgoat/container/service/LabelDebugService.java

--- src/main/java/org/owasp/webgoat/webwolf/FileServer.java
+++ src/main/java/org/owasp/webgoat/webwolf/FileServer.java
@@ -27,6 +27,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -56,7 +57,8 @@ public class FileServer {
   @RequestMapping(
       path = "/file-server-location",
       consumes = ALL_VALUE,
-      produces = MediaType.TEXT_PLAIN_VALUE)
+      produces = MediaType.TEXT_PLAIN_VALUE,
+      method = RequestMethod.GET)
   @ResponseBody
   public String getFileLocation() {
     return fileLocation;
