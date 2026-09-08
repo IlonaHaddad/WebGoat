d8e447690732d7f808a15e7f7a7c5b73a031e964
OX Security <api@ox.security>
2026-09-08 21:02:09 +0000

    Fix SQL Injection (SQLi) • Java • Taint Flow • Public Repo • API Exposed in src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/advanced/SqlInjectionChallenge.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/advanced/SqlInjectionChallenge.java
@@ -51,10 +51,10 @@ public class SqlInjectionChallenge implements AssignmentEndpoint {
     if (attackResult == null) {
 
       try (Connection connection = dataSource.getConnection()) {
-        String checkUserQuery =
-            "select userid from sql_challenge_users where userid = '" + username + "'";
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(checkUserQuery);
+        PreparedStatement preparedStatementCheck =
+            connection.prepareStatement("select userid from sql_challenge_users where userid = ?");
+        preparedStatementCheck.setString(1, username);
+        ResultSet resultSet = preparedStatementCheck.executeQuery();
 
         if (resultSet.next()) {
           attackResult = failed(this).feedback("user.exists").feedbackArgs(username).build();
